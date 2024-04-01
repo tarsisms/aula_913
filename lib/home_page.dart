eecd4db7b707e0ff2bca3fcfc826f54b09cfb6ce
tarsisms <tarsis.souza@ifal.edu.br>
2024-04-01 10:46:42 -0300

    Aula 07 - Interfaces Gráficas - 01/04

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,63 +12,126 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFF10397B),
-        title: Text(
+        backgroundColor: const Color(0xFF10397B),
+        title: const Text(
           'Pesquisar',
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Container(
-            margin: EdgeInsets.all(16),
-            color: Color(0xFFAD00D5), // Colors.deepPurple,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+          buildContainerPropaganda(),
+          buildCardPacote(),
+        ],
+      ),
+    );
+  }
+
+  buildContainerPropaganda() {
+    return Container(
+      margin: const EdgeInsets.all(16),
+      color: const Color(0xFFAD00D5), // Colors.deepPurple,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const SizedBox(width: 8),
+          Expanded(
+            child: Column(
               children: [
-                SizedBox(width: 8),
-                Column(
-                  children: [
-                    Text(
-                      'Top destinos mais buscados',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600),
-                    ),
-                    Text(
-                      'Corre que tá rolando muita promoção',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        'EU QUERO!',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.yellow),
-                    ),
-                  ],
+                const Text(
+                  'Top destinos mais buscados',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-                SizedBox(width: 8),
-                Image.network(
-                  'https://img.freepik.com/fotos-gratis/casal-multirracial-com-bola-posando-na-praia_23-2148137988.jpg',
-                  height: 120,
+                const Text(
+                  'Corre que tá rolando muita promoção',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'EU QUERO!',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                  ),
                 ),
               ],
             ),
           ),
+          const SizedBox(width: 8),
+          Image.network(
+            'https://img.freepik.com/fotos-gratis/casal-multirracial-com-bola-posando-na-praia_23-2148137988.jpg',
+            height: 180,
+          ),
         ],
       ),
     );
+  }
+
+  buildCardPacote() {
+    return Card(
+        color: Colors.white,
+        margin: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+                'https://www.viagenscinematograficas.com.br/wp-content/uploads/2019/03/Cancun-Melhores-Praias-Riviera-Maya-Capa.jpg'),
+            const Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Pacote Cancún 2024',
+                    style: TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text('Aéreo + Hotel All inclusive'),
+                  Row(
+                    children: [
+                      Icon(Icons.wb_sunny_outlined, size: 20),
+                      SizedBox(width: 4),
+                      Text('5 Diárias'),
+                      SizedBox(width: 8),
+                      Icon(Icons.person, size: 20),
+                      SizedBox(width: 4),
+                      Text('1 Pessoa'),
+                    ],
+                  ),
+                  Text('A partir de R\$ 6816'),
+                  Text(
+                    'R\$ 3.749',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFFD6C00),
+                    ),
+                  ),
+                  Text(
+                    'Cancelamento Grátis',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.green),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
