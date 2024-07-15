03636c0294c5e62c02caf16e765c3d513579fe78
tarsisms <tarsis.souza@ifal.edu.br>
2024-07-15 11:22:37 -0300

    Aula 09 - Interfaces Gráficas - 15/07

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({super.key});
@@ -16,65 +17,129 @@ class _DetailPageState extends State<DetailPage> {
           Image.network(
             'https://www.viagenscinematograficas.com.br/wp-content/uploads/2019/03/Cancun-Melhores-Praias-Riviera-Maya-Capa.jpg',
           ),
-          const Padding(
-            padding: EdgeInsets.all(16),
+          Padding(
+            padding: const EdgeInsets.all(16),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   'CANCÚN, MEX',
-                  style: TextStyle(fontSize: 16),
+                  style: GoogleFonts.montserrat(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-                SizedBox(height: 8),
+                const SizedBox(height: 8),
                 Text(
                   'Pacote Cancún 2025',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  style: GoogleFonts.montserrat(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-                SizedBox(height: 8),
-                Text('Aereo + Hotel All Inclusive'),
-                SizedBox(height: 8),
-                Text(
-                  '5 ou 7 diárias',
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(height: 8),
+                const SizedBox(height: 8),
+                buildText('Aereo + Hotel All Inclusive'),
+                const SizedBox(height: 8),
+                buildText('5 ou 7 diárias'),
+                const SizedBox(height: 8),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      'Válido por um período de',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    Text(
-                      'A partir de R\$ 6816',
-                      style: TextStyle(fontSize: 15),
-                    ),
+                    buildText('Válido por um período de'),
+                    buildText('A partir de R\$ 6816'),
                   ],
                 ),
-                SizedBox(height: 8),
+                const SizedBox(height: 8),
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Expanded(
                       child: Text(
                         'De 01 jan. 2025 a 31 dez. 2025',
-                        style: TextStyle(fontSize: 22),
+                        style: GoogleFonts.montserrat(
+                          fontSize: 22,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
                     ),
-                    SizedBox(width: 8),
+                    const SizedBox(width: 8),
                     Text(
-                      'R\$ 3749',
-                      style: TextStyle(
+                      'R\$ 3.749',
+                      style: GoogleFonts.montserrat(
                           fontSize: 36,
                           fontWeight: FontWeight.bold,
-                          color: Color(0xFFFD6C00)),
+                          color: const Color(0xFFFD6C00)),
                     ),
                   ],
                 ),
+                const SizedBox(height: 16),
+                const Divider(),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Text(
+                        'O que está incluso',
+                        style: GoogleFonts.montserrat(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Text(
+                        'Cancelamento Grátis!',
+                        style: GoogleFonts.montserrat(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 24),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      children: [
+                        Icon(Icons.check, size: 48),
+                        Text('Apartamento'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.room_service, size: 48),
+                        Text('All Inclusive'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.flight, size: 48),
+                        Text('Passagem Aerea'),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 24),
+                const Divider(),
               ],
             ),
           )
         ],
+      ),
+    );
+  }
+
+  buildText(String text) {
+    return Text(
+      text,
+      style: GoogleFonts.montserrat(
+        fontSize: 15,
       ),
     );
   }
